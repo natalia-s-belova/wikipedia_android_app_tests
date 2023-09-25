dd7cff34f7d091e57fc8ba8f44ade09996e4f628
natalia-s-belova <natalya.s.belova@gmail.com>
2023-09-25 15:04:17 +0400

    Uncomment tests for the app

--- tests/test_wikipedia.py
+++ tests/test_wikipedia.py
@@ -1,23 +1,23 @@
 from wikipedia_android_app_tests.models.wikipedia_app import Wikipedia
 
 
-# def test_tutorial_shown_and_functional_after_installation():
-#     application = Wikipedia()
-#     application.verify_tutorial_presented()
-#     application.interact_and_complete_tutorial()
-#
-#
-# def test_skip_tutorial():
-#     application = Wikipedia()
-#     application.skip_tutorial()
-#     application.verify_tutorial_not_presented()
-#
-#
-# def test_search_article_by_title():
-#     application = Wikipedia()
-#     application.skip_tutorial()
-#     application.search_article('Appium')
-#     application.verify_article_title_in_list('Appium')
+def test_tutorial_shown_and_functional_after_installation():
+    application = Wikipedia()
+    application.verify_tutorial_presented()
+    application.interact_and_complete_tutorial()
+
+
+def test_skip_tutorial():
+    application = Wikipedia()
+    application.skip_tutorial()
+    application.verify_tutorial_not_presented()
+
+
+def test_search_article_by_title():
+    application = Wikipedia()
+    application.skip_tutorial()
+    application.search_article('Appium')
+    application.verify_article_title_in_list('Appium')
 
 
 def test_open_found_article():
